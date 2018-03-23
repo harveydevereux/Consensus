f2ef66d42168f6fb6d2326aea4712d202e7ecca0
harveydevereux <harveydevereux@googlemail.com>
2018-03-23 22:41:42 +0000

    minor + finished example

--- dynamics.py
+++ dynamics.py
@@ -6,7 +6,7 @@ def distributed(x,L):
        It's actually gradient descent of 1/2(xLx)"""
     return -np.dot(L,x)
 
-def distributed_random_topology(x,graph, proportion):
+def distributed_random_topology(x,graph, proportion=0.5):
     """Same as dynamics but randomly rewires
        the graph edge connections."""
     nx.connected_double_edge_swap(graph,np.floor(proportion*len(graph.nodes())))

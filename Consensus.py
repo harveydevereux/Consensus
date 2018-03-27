393f899dbcbda0e5579fa3c5e904b4cc74e301e9
harveydevereux <harveydevereux@googlemail.com>
2018-03-27 11:51:42 +0100

    delay

--- Consensus.py
+++ Consensus.py
@@ -12,7 +12,8 @@ class Consensus_Simulation:
                  dynamics_args,
                  time_step=0.01,
                  x_init=None,
-                 convergence_warning=True):
+                 convergence_warning=True,
+                 delay=0):
         # check arguments are of the
         # correct form
         if(isinstance(topology,nx.Graph)):
@@ -29,6 +30,7 @@ class Consensus_Simulation:
         else:
             print("Argument Error: dynamics must be a function")
         self.dt = time_step
+        self.tau = delay
         # set up initial vector to
         # 1,2,3,...,n
         if(x_init==None):
@@ -44,6 +46,9 @@ class Consensus_Simulation:
         # maybe there's some algorithm that will
         # though...
         self.warn = convergence_warning
+
+        self.d_max = max(np.array(self.graph.degree)[:,1])
+        self.tau_max = (np.pi)/(4*self.d_max)
 
     def disagreement(self):
         """Returns the 'error'/in-homogeneity in the
@@ -87,6 +92,66 @@ class Consensus_Simulation:
             t = t+self.dt
         end = time.time()
 
+    def run_sim_delay(self):
+        """Simulates the algorithm with a time delay of
+            int(tau) milliseconds"""
+        if(self.tau == 0):
+            return self.run_sim()
+        t=0
+        # could be re-set
+        self.x = self.x_init.copy()
+        self.X = list()
+        self.T = list()
+        flag = False
+
+        self.X.append(self.x_init)
+        self.T.append(0)
+        start = time.time()
+        while self.agreement() == False:
+            if(t==0 and self.warn and not nx.is_connected(self.graph)):
+                print("Graph not connected, consensus algorithm will probably not converge!")
+                print("Simulating to 5 seconds...")
+                flag = True
+            if(t==0 and self.tau*self.dt >= self.tau_max and self.warn):
+                print("Communication delay is to long, algorithm will probably not converge!")
+                print("Simulating to 5 seconds...")
+                flag = True
+            # if delay takes us to negative time
+            # do the normal thing
+
+            for i in range(1,self.size+1):
+                if(t - int(self.tau)*self.dt<0):
+                    break
+                else:
+                    x_tau = self.X[len(self.X)-1-int(self.tau)]
+                    dx = 0
+                    for v in self.graph.neighbors(i):
+                        if (v != i):
+                            dx = dx + x_tau[v-1]-x_tau[i-1]
+                    self.x[i-1] = self.x[i-1]+self.dt*dx
+
+
+            if(flag and time.time()-start>5):
+                break
+
+            self.X.append(self.x)
+            self.T.append(time.time()-start)
+            t = t+self.dt
+        end = time.time()
+
+        # get rid of non-integer times
+        # vastly reduces memory load
+        T=list()
+        X=list()
+        for i in range(0,len(self.T)):
+            if i % 10 == 0:
+                T.append(self.T[i])
+                X.append(self.X[i])
+        self.T = T
+        self.X = X
+        del T
+        del X
+
     def plot(self):
         """Show the convergence analysis"""
         if(len(self.X)==0 or len(self.T)==0):
@@ -101,3 +166,7 @@ class Consensus_Simulation:
         plt.ylabel("State")
         plt.title("Convergence of consensus algorithm")
         plt.legend()
+
+    def print_delay(self):
+        print("Delay in seconds")
+        return self.dt*self.tau
